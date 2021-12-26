c4b1829005ce23b891eba043e2234a842fe804a9
Tsitsi Munikwa <Tsitsi.Munikwa@cassavasmartech.co.zw>
2021-12-26 22:20:53 +0200

    Code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # JumoBot
 A slack bot for weather updates
+
+
+# Running the application locally
+
+> This guide assumes you have Python 3+ /w pip installed
+
+1. Activate your virtualenv.
+
+2. Install requirements using pip
+```bash
+> pip install -r requirements.txt
+```
+
+3. Run the app
+
+```bash
+> python3 jumbobot.py
+```
--- jumobot.py
+++ jumobot.py
@@ -1,10 +1,8 @@
-import os
-import requests
-import time
 import json
-
-from requests.models import Response
 import slack
+import requests
+import urllib.parse
+from requests.models import Response
 from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 from jumo_config import SLACK_BOT_TOKEN, WEATHER_API_TOKEN, BOT_NAME, SIGNING_SECRET
@@ -30,36 +28,57 @@ def message(payload):
     if BOT_ID != user_id:
         slack_client.chat_postMessage(channel=channel_id, text="That sounds awesome!")
 
+def get_data():
+    '''
+    Helper method for getting the data sent by Slack to our webhook
+    '''
+    
+    data = request.form
+
+    return (data.get('channel_id'), data.get('text'))    
 
 @app.route('/jumo_weather', methods=['POST'])
 def weather_request():
-
     '''Makes a get request to the Open Weather API, gets the weather of the
     requested city.'''
 
-    data = request.form
-    #print(data)
-    channel = data.get('channel_id')
-    city = data.get('text')
-    #print(city)
-    
-    api_key = WEATHER_API_TOKEN
-    route = f'://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
-    api_request = requests.get('https' + route)
-    data = json.loads(api_request.text)
-    temp = data['main']['temp']
-    feels_like = data['main']['feels_like']
-    name = data['name']
-    weather_response = f'The temperature is {temp} and it feels like {feels_like} degrees in {name}.'
-    #print(weather_response)
+    # Get the data sent through the post request
+    channel, city = get_data()
+
+    # URL encode user input
+    city = urllib.parse.quote_plus(city)
+
+    # Build up the URL for the OWM API, passing in the city name and our API token
+    route = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={WEATHER_API_TOKEN}'
+
+    response = ''
+
+    try:
+        # Send a request to the API
+        api_request = requests.get(route)
+
+        # Parse JSON data
+        data = json.loads(api_request.text)
+
+        # If the API returns malformed data, it's an indicator that the city might not exist, send the message to the user
+        if data['cod'] is not 200:
+            response = data['message'] or 'An error occured while proccessing your request'
+        
+        else:
+            # Build response string
+            response = 'The temperature is {} and it feels like {} degrees in {}.'.format( 
+                data['main']['temp'], data['main']['feels_like'], data['name'] 
+            )
+        
+    except Exception as e:
+        print(str(e))
+        response = 'Something went wrong, please try again.'
+
 
     '''Takes the response from the weather and posts it in the channel'''
 
-    #default response for when the weather API does not understand the request
-    default_response = 'City not found, please try again.'
-
     #sending the response back to the channel
-    slack_client.chat_postMessage(channel=channel, text=weather_response or default_response)
+    slack_client.chat_postMessage(channel=channel, text=response.capitalize())
      
     return Response(), 200
 
@@ -67,5 +86,5 @@ if __name__=="__main__":
     app.run(debug=True, port=8000)
 
 
-# todo: get temperature in degrees celcius
-# fix the issue with ngrok
+
+# todo: fix the issue with ngrok
